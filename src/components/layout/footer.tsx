317f558e92a7efa26234fba836964f9c73ba521e
Hanzo Web <webhanzo754@gmail.com>
2025-11-04 01:27:36 +0000

    احذف (_for element p_)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -7,9 +7,6 @@ export default function Footer() {
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <UtensilsCrossed className="h-6 w-6 text-primary" />
-          <p className="text-center text-sm leading-loose md:text-left">
-            Built by your friends at Firebase.
-          </p>
         </div>
         <p className="text-center text-sm text-muted-foreground">
             © {new Date().getFullYear()} WeEat. All rights reserved.
