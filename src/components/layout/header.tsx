fc80556af9a927589154ad4b71807f7cb20a6e5f
Hanzo Web <webhanzo754@gmail.com>
2025-11-04 02:40:35 +0000

    اجعلها Create Order (_for element <LinkComponent>_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
                     <Link href="/">Home</Link>
                 </Button>
                 <Button asChild variant="ghost">
-                    <Link href="/orders/create">انشاء طلب</Link>
+                    <Link href="/orders/create">Create Order</Link>
                 </Button>
                 <Button asChild variant="ghost">
                     <Link href="/orders/history">History</Link>
